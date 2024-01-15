c6b96d09408af82661182a84dc54b9915de6569d
Diego Jara <djara@ninjaexcel.com>
2024-01-15 16:08:43 -0300

    cambio en app.

--- config/app.php
+++ config/app.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://ceva-system-web-db.azurewebsites.net'),
+    'url' => env('APP_URL', 'http://localhost'),
 
     'asset_url' => env('ASSET_URL'),
 
