6cc0e82a900424f7f705e8fe69cf0045ddd01c81
Diego Jara <djara@ninjaexcel.com>
2024-01-23 10:46:03 -0300

    prueba ejecución

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
          //$schedule->command('moneda:create')->everyMinute();
-         $schedule->command('proceso:trabajadores')->dailyAt('13:43');
+         $schedule->command('proceso:trabajadores')->dailyAt('13:55');
         //$schedule->call([ApiGeoController::class, 'processTrabajadores'])->dailyAt('01:00');
     }
 
