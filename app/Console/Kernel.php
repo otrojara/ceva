b8c8a6521a6b30626bb6ec448cb282eb0d1d59d9
Diego Jara <djara@ninjaexcel.com>
2024-01-28 17:59:12 -0300

    cargando en aws

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
          //$schedule->command('moneda:create')->everyMinute();
-        $schedule->command('queue:work')->everyMinute();
+       // $schedule->command('queue:work')->everyMinute();
          $schedule->command('proceso:trabajadores')->dailyAt('14:55');
         //$schedule->call([ApiGeoController::class, 'processTrabajadores'])->dailyAt('01:00');
     }
