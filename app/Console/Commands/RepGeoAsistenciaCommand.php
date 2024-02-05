541d2e6986db596530fc6591224a8e31b51e478a
Diego Jara <djara@ninjaexcel.com>
2024-02-05 08:17:56 -0300

    aaa

--- app/Console/Commands/RepGeoAsistenciaCommand.php
+++ app/Console/Commands/RepGeoAsistenciaCommand.php
@@ -19,7 +19,7 @@ class RepGeoAsistenciaCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'proceso:xptres';
+    protected $signature = 'proceso:yptres';
 
     /**
      * The console command description.
