105e4a5693173bd405a3cf9a41319319bb43a210
Diego Jara <djara@ninjaexcel.com>
2024-02-19 20:27:07 -0300

    aa

--- app/Console/Commands/RepGeoAsistenciaCommand.php
+++ app/Console/Commands/RepGeoAsistenciaCommand.php
@@ -19,7 +19,7 @@ class RepGeoAsistenciaCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'proceso:dptres';
+    protected $signature = 'proceso:eptres';
 
     /**
      * The console command description.
