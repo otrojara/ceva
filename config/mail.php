935ceae5d966cd055d45e495e07e92c61457407f
Diego Jara <djara@ninjaexcel.com>
2024-02-25 15:23:59 -0300

    aa

--- config/mail.php
+++ config/mail.php
@@ -37,15 +37,14 @@ return [
         'smtp' => [
             'transport' => 'smtp',
             'url' => env('MAIL_URL'),
-            'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
-            'port' => env('MAIL_PORT', 587),
-            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
-            'username' => env('MAIL_USERNAME'),
-            'password' => env('MAIL_PASSWORD'),
+            'host' => env('mail.workflowsystem.cl', 'smtp.mailgun.org'),
+            'port' => env(465, 587),
+            'encryption' => env('ssl', 'tls'),
+            'username' => env('alerta@workflowsystem.cl'),
+            'password' => env('Sistema2023.'),
             'timeout' => null,
             'local_domain' => env('MAIL_EHLO_DOMAIN'),
         ],
-
         'ses' => [
             'transport' => 'ses',
         ],
