2c24631718e2606aae10fe72c4cbfc497f53eaee
Diego Jara <djara@ninjaexcel.com>
2024-04-18 10:18:44 -0400

    carga marzo

--- app/Console/Commands/RepGeoAsistenciaCommand.php
+++ app/Console/Commands/RepGeoAsistenciaCommand.php
@@ -19,7 +19,7 @@ class RepGeoAsistenciaCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'proceso:cevarepmarsiete';
+    protected $signature = 'proceso:cevarepmarocho';
 
     /**
      * The console command description.
