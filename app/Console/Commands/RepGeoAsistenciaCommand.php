b58538f7dc12082d677d907d295dba8073678dba
Diego Jara <djara@ninjaexcel.com>
2024-04-21 20:02:18 -0400

    a

--- app/Console/Commands/RepGeoAsistenciaCommand.php
+++ app/Console/Commands/RepGeoAsistenciaCommand.php
@@ -19,7 +19,7 @@ class RepGeoAsistenciaCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'proceso:reuno';
+    protected $signature = 'proceso:redos';
 
     /**
      * The console command description.
