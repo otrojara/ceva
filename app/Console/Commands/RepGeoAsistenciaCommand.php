167486de3d7a6af05f74cd98b116f0392f3940fb
Diego Jara <djara@ninjaexcel.com>
2024-04-21 22:05:09 -0400

    a

--- app/Console/Commands/RepGeoAsistenciaCommand.php
+++ app/Console/Commands/RepGeoAsistenciaCommand.php
@@ -19,7 +19,7 @@ class RepGeoAsistenciaCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'proceso:retres';
+    protected $signature = 'proceso:recuatro';
 
     /**
      * The console command description.
--- app/Jobs/RepGeoAsistenciaJob.php
+++ app/Jobs/RepGeoAsistenciaJob.php
@@ -33,11 +33,11 @@ class RepGeoAsistenciaJob implements ShouldQueue
 
         $fecha = Carbon::parse(Carbon::now())->format('Y-m-d');
 
-        $fecha = '2024-03-29';
+        $fecha = '2024-04-09';
          //RepGeoAsistencia::truncate();
 
         $fechaAnt = Carbon::now()->subDays(7)->format('Y-m-d');
-        $fechaAnt = '2024-03-20';
+        $fechaAnt = '2024-03-30';
 
         RepGeoAsistencia::where('fecha', '>=', $fechaAnt)->delete();
 
